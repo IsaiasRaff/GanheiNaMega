8997037025571bdbcbd87d1642c80d5122234d3c
IsaiasRaff <isaiasrafael01@gmail.com>
2026-04-28 17:32:07 -0300

    Refactor: atualização completa do projeto (manifest, gradle, correções)

--- app/src/androidTest/java/co/isaiasraff/ganheinamega/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/co/isaiasraff/ganheinamega/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package co.tiagoaguiar.ganheinamega
+package co.isaiasraff.ganheinamega
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
